87417c7c6f42c429fb5e053115325635f1be3878
Faizan Khan <muhammadfaizankhan525@gmail.com>
2026-09-11 22:02:40 +0500

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/aplinode-clone">
       <App />
     </BrowserRouter>
   </React.StrictMode>
